a51ce75d5434b9736d7938a61935935cf1f52730
Vinay Yadav <139154513+vinayyadav101@users.noreply.github.com>
2026-02-06 14:25:24 +0530

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,8 @@
+import mysql from "mysql2/promise";
+
+export const db = mysql.createPool({
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "",
+  database: process.env.MYSQL_DATABASE || "task_manager",
+});
